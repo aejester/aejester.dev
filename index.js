e8d5b4380b37642f16a15edf53c86b2aef8759fc
AEJester <aftereffectsjester@gmail.com>
2020-03-29 16:37:07 -0400

    first version

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const http = require("http").Server(app);
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+
+    res.render(__dirname+"/homepage/index.ejs");
+
+});
+
+app.get("/images/:image", (req, res) => {
+    console.log(req.params)
+    res.sendFile(__dirname+"/images/"+req.params.image);
+})
+
+http.listen(process.env.PORT || 8000, () => {
+    console.log("listening on *:"+(process.env.PORT || 8000));
+});
